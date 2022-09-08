c3f06d3a80ab6a316f19ff7ae7728ff025b95bd0
venadda <venadda@gmail.com>
2022-09-08 10:53:09 +0530

    requirements static root

--- polki_dolki/polki_dolki/settings.py
+++ polki_dolki/polki_dolki/settings.py
@@ -1,7 +1,7 @@
 
 from pathlib import Path
 import os
-import django_heroku
+import django-heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
