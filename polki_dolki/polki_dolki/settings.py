8566f3e2ce8e13a07bb52e5d15f1cb43ca812e3a
venadda <venadda@gmail.com>
2022-09-08 10:31:29 +0530

    added static root

--- polki_dolki/polki_dolki/settings.py
+++ polki_dolki/polki_dolki/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
